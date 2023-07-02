857be46ab6ed3df903ef0f4e039fe912d245cf38
Aashish Gupta <aashishg863@gmail.com>
2023-07-02 18:27:19 +0530

    Weather App

--- script.js
+++ script.js
@@ -6,10 +6,14 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function fetchWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+   
+    if(response.status==404 )
+    {
+        document.querySelector(".err").style.display="block";
+        document.querySelector(".weather").style.display="none";
+    } 
+    else{
     var data = await response.json();
-
-
-    console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML =Math.round( data.main.temp) + "°C";
@@ -31,6 +35,7 @@ async function fetchWeather(city) {
         weatherIcon.src = "images/mist.png";
     }
 
+}
 }
 searchBtn.addEventListener("click", () => {
     fetchWeather(searchBox.value);
